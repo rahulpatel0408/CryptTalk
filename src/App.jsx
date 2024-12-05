3432bac81e7724bc9d09fcfdc6ae288448842c35
rahulpatel0408 <147559135+rahulpatel0408@users.noreply.github.com>
2024-12-05 11:58:53 +0530

    Protected Routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import React,{lazy} from "react";
+import React, { lazy } from "react";
 import "./App.css";
 
 
-const Home = lazy(()=>import("./pages/Home"));
-const Login = lazy(()=>import("./pages/Login"));
-const Register = lazy(()=>import("./pages/Register"));
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const ProtectRoute = lazy(() => import("./components/auth/ProtectRoute"));
 
+let user = false;
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+
+          <Route element={<ProtectRoute user={user} />} >
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route path="/login" element={
+            <ProtectRoute user={!user} redirect="/" children={<Login />} />
+          } />
+          <Route path="/register" element={
+            <ProtectRoute user={!user} redirect="/" children={<Register />} />
+          } />
+          <Route path="*" element={<Home />} />
+
         </Routes>
       </BrowserRouter>
     </>
