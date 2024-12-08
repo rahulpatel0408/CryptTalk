3070235e08d12ad45f66d537d6050e122e74e3f7
rajajtiitdhn <rksaw3232@gmail.com>
2024-12-08 14:05:03 +0530

    Search page added

--- src/components/constants/SampleData2.jsx
+++ src/components/constants/SampleData2.jsx
@@ -1,17 +1,37 @@
-
 export const sampleChats = [
-{
-    avatar:["https://tse4.mm.bing.net/th?id=OIP.w1sMHUdLIzuHF6vaS9RRygHaIy&pid=Api&P=0&h=180"],
-    name:"Rajat",
-    _id:"1",
-    groupChat:false,
-    members:["1","2"],
-},
-{
-    avatar:["https://tse4.mm.bing.net/th?id=OIP.w1sMHUdLIzuHF6vaS9RRygHaIy&pid=Api&P=0&h=180"],
-    name:"Rahul",
-    _id:"2",
-    groupChat:true,
-    members:["1","2"],
-}
-]
+  {
+    avatar: [
+      "https://tse4.mm.bing.net/th?id=OIP.w1sMHUdLIzuHF6vaS9RRygHaIy&pid=Api&P=0&h=180",
+    ],
+    name: "Rajat",
+    _id: "1",
+    groupChat: false,
+    members: ["1", "2"],
+  },
+  {
+    avatar: [
+      "https://tse4.mm.bing.net/th?id=OIP.w1sMHUdLIzuHF6vaS9RRygHaIy&pid=Api&P=0&h=180",
+    ],
+    name: "Rahul",
+    _id: "2",
+    groupChat: true,
+    members: ["1", "2"],
+  },
+];
+
+export const sampleUsers = [
+  {
+    avatar: [
+      "https://tse4.mm.bing.net/th?id=OIP.w1sMHUdLIzuHF6vaS9RRygHaIy&pid=Api&P=0&h=180",
+    ],
+    name: "Rajat",
+    _id: "1",
+  },
+  {
+    avatar: [
+      "https://tse4.mm.bing.net/th?id=OIP.w1sMHUdLIzuHF6vaS9RRygHaIy&pid=Api&P=0&h=180",
+    ],
+    name: "Rahul",
+    _id: "2",
+  },
+];
---
+++ src/components/shared/UserItem.jsx
@@ -0,0 +1,47 @@
+import { Avatar, IconButton, ListItem, Stack, Typography } from "@mui/material";
+import React, { memo } from "react";
+import {Add as AddIcon} from "@mui/icons-material"
+const UserItem = ({ user, handler, handlerIsLoading }) => {
+  const { name, _id, avatar } = user;
+  return (
+    <ListItem>
+      <Stack
+        direction={"row"}
+        alignItems={"center"}
+        spacing={"1rem"}
+        width={"100%"}
+      >
+        <Avatar />
+        <Typography
+          variant="body1"
+          sx={{
+            flexGrow: 1,
+            display: "-webkit-box",
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            width:"100%"
+          }}
+        >
+          {name}
+        </Typography>
+        <IconButton
+        size="small"
+        sx={{
+            bgcolor:"primary.maoin",
+            color:"white",
+            "&:hover":{
+                bgcolor:"primary.dark",
+            },
+        }}
+        onClick={() => handler(_id)} disabled={handlerIsLoading}>
+          <AddIcon sx={{
+            color:"blue"
+          }}/>
+        </IconButton>
+      </Stack>
+    </ListItem>
+  );
+};
+
+export default memo(UserItem);
